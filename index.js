3cf2203607e359828a1d3d2653f04328f2a9ca7c
MargaritkaN <74735077+MargaritkaN@users.noreply.github.com>
2021-02-03 22:21:24 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+const manName = 'Чендлер';
+const womanName = 'Моника';
+const dogName = 'Добби';
+let nameOfHouse = 'Sweet home';
+let inhabitants ='2'
+const rack = 'Стеллаж';
+const sofa = 'Диван';
+const armchair = 'Кресло';
+const stool = 'Табурет';
+const coffeTable = 'Журнальный столик';
+let lamps = '2';
+let books = '10';
+let dishes = '14';
+let pottedFlowers = '3';
+let carpet = '1';
+let vases = '8';
+let pillows = '2'
+let windows = undefined;
+let doors = undefined;
+let rooms = undefined;
+console.log(`Добро пожаловать в дом ${womanName} и ${manName} , а также их собаки ${dogName}!`);
+console.log(`В квартире живет ${inhabitants} человек. Их зовут ${womanName} и ${manName}`);
+console.log(`В комнате есть следующая мебель: ${rack},${sofa},${armchair},${stool},${coffeTable}`);
+console.log(`И столько элементов декора:${lamps}+${books}+${dishes}+${pottedFlowers}+${carpet}+${vases}+${pillows}`);
+console.log(`У нас есть следующие типы переменных:${typeof manName}, ${typeof dishes}, ${typeof rooms}.`);
+
+
+
+
+
+
+
+
+
