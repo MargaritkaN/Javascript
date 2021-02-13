74a88190ad5a6fd7b1e231c06e6f5acf1beb42c6
MargaritkaN <74735077+MargaritkaN@users.noreply.github.com>
2021-02-13 16:47:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const manName = 'Чендлер';
 const womanName = 'Моника';
 const dogName = 'Добби';
 let nameOfHouse = 'Sweet home';
-let inhabitants ='2'
+let inhabitants = 2;
 const rack = 'Стеллаж';
 const sofa = 'Диван';
 const armchair = 'Кресло';
@@ -25,8 +25,38 @@ console.log(`В комнате есть следующая мебель: ${rack}
 console.log(`И столько элементов декора:${lamps}+${books}+${dishes}+${pottedFlowers}+${carpet}+${vases}+${pillows}`);
 console.log(`У нас есть следующие типы переменных:${typeof manName}, ${typeof dishes}, ${typeof rooms}.`);
 
+let homeType;
+if(inhabitants<=2){
+   homeType ='Это студия';
+}
+else{
+    homeType ='Это многокомнатная квартира';
+}
+alert(homeType);
 
+let firstName;
+let secondName;
+if(manName>womanName) {
+    firstName=womanName;
+    secondName=manName;
+}
+else{
+    firstName=manName;
+    secondName=womanName;
+}
+alert(`Добро пожаловать в дом ${firstName} и ${secondName}, а также их собаки ${dogName}!`);
 
+let decor;
+if (lamps+books+dishes+pottedFlowers+carpet+vases+pillows<20) {
+    decor = 'Квартира в стиле минимализм';  
+}
+else if(lamps+books+dishes+pottedFlowers+carpet+vases+pillows>=20 && lamps+books+dishes+pottedFlowers+carpet+vases+pillows<=40){
+    decor = 'Квартира в стиле модерн';
+}
+else{
+    decor = 'Квартира в стиле классика';
+}
+alert(decor);
 
 
 
